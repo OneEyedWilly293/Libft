f64bd9a05fc4139a154b321aab6c234c7ee9511f
OneEyedWilly293clear <danielinkmou@gmail.com>
2025-04-15 15:52:24 +0300

    test push

---
+++ ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgueon <jgueon@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 18:09:29 by jgueon            #+#    #+#             */
+/*   Updated: 2025/04/14 18:23:52 by jgueon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	result = 0;
+	while(*str == ' ' || *str >= 9 && *str <= 13)
+		str++;
+
+	if (*str == '-' || *str == '+')
+	{
+		if(*str == '-')
+		{
+			sign *= -1;
+		}
+	}
+	while(*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result * sign);
+}
